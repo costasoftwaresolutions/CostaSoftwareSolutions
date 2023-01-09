8468ba39cc94758d1b4cdc8b486993b117c65ccd
carloseadac <carloseadac@gmail.com>
2023-01-09 20:23:05 -0300

    agora vai

--- email.php
+++ email.php
@@ -1,9 +1,9 @@
 <?php
 
     if(isset($_POST['email']) && !empty('email')){
-        $nome = addslashes($_POST['nome'])
-        $email = addslashes($_POST['email'])
-        $mensagem = addcslashes($_POST['mensagem'])
+        $nome = addslashes($_POST['nome']);
+        $email = addslashes($_POST['email']);
+        $mensagem = addslashes($_POST['mensagem']);
 
         $to = "costa.carlos.dev@gmail.com";
         $subject = "Contato Site - Costa Software Solutions";
@@ -12,7 +12,7 @@
                 "Mensagem: ".$mensagem;
         $header = "From:contato@costasoftwaresolutions.com"."\r\n"
                     ."Reply-To:".$email."\r\n"
-                    .X=Mailer:PHP/".phpversion();
+                    ."X=Mailer:PHP/".phpversion();
 
         if(mail($to,$subject,$body,$header)){
             echo("Email enviado com sucesso");
